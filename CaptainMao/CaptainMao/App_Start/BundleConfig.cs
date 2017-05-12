287b30c5cac297ba6d44d82e797e0bc7ade22d4c
Aslan <tom7826@hotmail.com>
2017-05-12 17:14:41 +0800

    主板

---
+++ CaptainMao/CaptainMao/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Login
+{
+    public class BundleConfig
+    {
+        // 如需「搭配」的詳細資訊，請瀏覽 http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/jquery-{version}.js", 
+                        "~/Scripts/bootstrap.js", 
+                        "~/Scripts/modernizr-2.6.2.js",
+                        "~/Scripts/JavaScript.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/reset.css",
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            
+
+        }
+    }
+}
---
+++ CaptainMao/CaptainMao/Controllers/CaptainMaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaptainMao.Controllers
+{
+    public class CaptainMaoController : Controller
+    {
+        // GET: CaptainMao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
