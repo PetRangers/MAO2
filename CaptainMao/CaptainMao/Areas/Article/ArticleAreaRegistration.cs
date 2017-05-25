ba07e9ed5d487b6f1ab26f5620c3c4b0ff5f5bfd
Link <b87563@gmail.com>
2017-05-25 17:33:38 +0800

    new

---
+++ CaptainMao/CaptainMao/Areas/Article/ArticleAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CaptainMao.Areas.Article
+{
+    public class ArticleAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Article";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Article_default",
+                "Article/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
