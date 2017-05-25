785a3c6ee7abe6b00ca4bc05fe089de2177d0267
Waiting18x27 <iop1x71@gmail.com>
2017-05-25 18:22:51 +0800

    修

---
+++ CaptainMao/CaptainMao/Areas/Adoption/AdoptionAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CaptainMao.Areas.Adoption
+{
+    public class AdoptionAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Adoption";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Adoption_default",
+                "Adoption/{controller}/{action}/{id}",
+                new { controller="Action", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
