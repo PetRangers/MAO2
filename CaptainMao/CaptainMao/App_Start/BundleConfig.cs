c252136e533b627360ce7071ec749cb6f6f34ae2
Aslan <tom7826@hotmail.com>
2017-05-25 14:00:21 +0800

    MAO project

---
+++ CaptainMao/CaptainMao/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Login
+{
+    public class BundleConfig
+    {
+        // 如需「搭配」的詳細資訊，請瀏覽 http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/jquery-{version}.js", 
+                        "~/Scripts/bootstrap.js", 
+                        "~/Scripts/modernizr-2.6.2.js",
+                        "~/js/JavaScript.js"));
+
+            bundles.Add(new StyleBundle("~/css").Include(
+                      "~/css/reset.css",
+                      "~/Content/bootstrap.css",
+                      "~/css/StyleSheet_Master.css"
+                      ));
+
+            
+
+        }
+    }
+}
---
+++ CaptainMao/CaptainMao/obj/Release/Package/PackageTmp/Scripts/JavaScript.js
@@ -0,0 +1,60 @@
+﻿
+$(function () {
+    /* smooth scrolling for scroll to top */
+    $('#to-top').click(function () {
+        $('body,html').animate({ scrollTop: 0 }, 1500);
+    });
+});
+    //Easing Scroll replace Anchor name in URL and Offset Position
+$(function () {
+    $('a[href*=\\#]:not([href=\\#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - 420
+                }, 3500, 'easeOutBounce');
+                return false;
+            }
+        }
+    });
+});
+
+
+$(document).ready(function () {
+
+    $(".dropdown").hover(funDown,funUP);
+
+
+    function funDown() {
+        $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideDown('400');
+     $(this).toggleClass('open');
+ }
+    function funUP() {
+        $('.dropdown-menu', this).not('.in .dropdown-menu').slideUp('400');
+     $(this).toggleClass('open');
+ }
+});
+
+
+$(function () {
+
+    $('#menu-toggle').mouseenter(fun_active).click(fun_active);
+    $('#sidebar-wrapper').mouseleave(fun_uactive);
+    $('#menu-close').click(fun_uactive);
+
+    var sidebar_wrapper = $("#sidebar-wrapper");
+
+    function fun_active(e) {
+        sidebar_wrapper.toggleClass('active', true);
+        e.preventDefault();
+    }
+    function fun_uactive(e) {
+        sidebar_wrapper.toggleClass('active', false);
+        e.stopPropagation();
+        e.preventDefault();
+    }
+
+});
+
