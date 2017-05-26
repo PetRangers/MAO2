3b75d06e35100feb4692b049f2022712c99fc35b
Aslan <tom7826@hotmail.com>
2017-05-26 17:38:29 +0800

    change2

---
+++ CaptainMao/CaptainMao/Areas/buy/buyAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CaptainMao.Areas.buy
+{
+    public class buyAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "buy";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "buy_default",
+                "buy/{controller}/{action}/{id}",
+                new { controller="Shopping", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
--- CaptainMao/CaptainMao/Controllers/CaptainMaoController.cs
+++ CaptainMao/CaptainMao/Controllers/CaptainMaoController.cs
@@ -25,5 +25,7 @@ namespace CaptainMao.Controllers
         {
             return View();
         }
+
+
     }
 }
