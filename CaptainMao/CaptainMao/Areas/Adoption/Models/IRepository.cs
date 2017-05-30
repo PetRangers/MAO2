ce7123371ad719cb5e692cc866e9403c1558613f
Waiting18x27 <iop1x71@gmail.com>
2017-05-30 09:39:18 +0800

    Adoption

---
+++ CaptainMao/CaptainMao/Areas/Adoption/Models/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptainMao.Areas.Adoption.Models
+{
+    public interface IRepository<T>
+    {
+        T GetByID(int id);
+        IEnumerable<T> GetAll();
+        void Create(T _entity);
+        void Update(T _entity);
+        void Delete(T _entity);
+    }
+}
