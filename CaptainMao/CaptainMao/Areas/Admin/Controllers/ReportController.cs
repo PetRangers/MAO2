4247468240df071a68116fb59ab8efb74dcda76b
Waiting18x27 <iop1x71@gmail.com>
2017-06-14 16:18:46 +0800

    合併

---
+++ CaptainMao/CaptainMao/Areas/Admin/Controllers/ReportController.cs
@@ -0,0 +1,40 @@
+﻿using CaptainMao.Areas.Adoption.ViewModel;
+using CaptainMao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaptainMao.Areas.Admin.Controllers
+{
+    public class ReportController : Controller
+    {
+        MaoEntities db = new MaoEntities();
+
+        // GET: Admin/Report
+        public ActionResult AdoptionReport()
+        {
+            return View();
+        }
+        public ActionResult GetAdoptionReport()
+        {
+            AdpReportViewModel data = new AdpReportViewModel();
+
+            List<AdpReport> adps = new List<AdpReport>();
+            int Total = db.Adoptions.Count();
+            int CateCount = db.Categories.Count();
+            for (int i = 1; i <= CateCount; i++)
+            {
+                AdpReport adp = new AdpReport();
+                adp.CategoryName = db.Categories.Where(a => a.CategoryID == i).Select(a => a.CategoryName).First();
+                double count = db.Adoptions.Where(a => a.CategoryID == i).Count();
+                adp.Share = count / Total;
+                adps.Add(adp);
+            }
+            data.AdpReports = adps;
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- CaptainMao/CaptainMao/Areas/Adoption/Controllers/AdoptionController.cs
+++ CaptainMao/CaptainMao/Areas/Adoption/Controllers/AdoptionController.cs
@@ -347,18 +347,5 @@ namespace CaptainMao.Areas.Adoption.Controllers
             return File(img, "image/jpeg");
         }
 
-        //private ApplicationUserManager _userManager;
-        //public ApplicationUserManager UserManager
-        //{
-        //    get
-        //    {
-        //        return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
-        //    }
-        //    private set
-        //    {
-        //        _userManager = value;
-        //    }
-        //}
-
     }
 }
