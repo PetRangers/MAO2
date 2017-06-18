1585fcb74b14639924a36cf6a27b9bee699811ba
Ta-Chun Chien <jdj0125@gmail.com>
2017-06-19 01:23:28 +0800

    啟用reCaptcha。 嘗試做圖表。

--- CaptainMao/CaptainMao/Areas/Admin/Controllers/WebMonitorController.cs
+++ CaptainMao/CaptainMao/Areas/Admin/Controllers/WebMonitorController.cs
@@ -11,6 +11,23 @@ namespace CaptainMao.Areas.Admin.Controllers
         // GET: Admin/WebMonitor
         public ActionResult Monitor()
         {
+            //string[] xAxis = new string[]{"a", "b", "c", "d", "e", "f" };
+            ViewBag.xAxis[0] = "a";
+            ViewBag.xAxis[1] = "b";
+            ViewBag.xAxis[2] = "c";
+            ViewBag.xAxis[3] = "d";
+            ViewBag.xAxis[4] = "e";
+            ViewBag.xAxis[5] = "f";
+
+            //int[] yAxis = new int[]{2, 4, 6, 7, 5, 1 };
+            ViewBag.yAxis[0] = 1;
+            ViewBag.yAxis[1] = 2;
+            ViewBag.yAxis[2] = 3;
+            ViewBag.yAxis[3] = 4;
+            ViewBag.yAxis[4] = 5;
+            ViewBag.yAxis[5] = 6;
+
+
             return View();
         }
     }
